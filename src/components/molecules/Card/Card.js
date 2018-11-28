0d9d3301ebc51ac00d51217a4247d037677734f8
Gornyy Vladimir <vladimir.gornyy@flatstack.com>
2018-11-28 19:57:25 +0300

    Add markup for card

--- src/components/molecules/Card/Card.js
+++ src/components/molecules/Card/Card.js
@@ -1,17 +1,19 @@
 import React from 'react';
+import classnames from 'classnames';
 import styles from './Card.module.css';
 
-const Card = ({ accountNumber, quantity, percent, createdAt, lastOperation, lastOperationDiff, currencySymbol = "₽"}) => {
+const Card = ({ className, accountNumber, money, percent, createdAt, lastOperation, lastOperationDiff, currencySymbol = "₽"}) => {
+  const classes = classnames(styles.wrapper, className);
   return (
-    <div className={styles.wrapper}>
-      <div className={styles.titleWrapper}>
-        <h3 className={styles.accountNumber}>{`Счет № ${accountNumber}`}</h3>
-        <button>Full size</button>
+    <div className={classes}>
+      <div className={styles.headerWrapper}>
+        <h3 className={styles.accountNumber}>{`Счет № ${accountNumber} `}</h3>
+        <button className={styles.maximizeButton} onClick={() => console.log("Clcik")}>Full size</button>
       </div>
-      <strong>{`${quantity} ${currencySymbol}`}</strong>
-      <span>{`${percent}% годовых`}</span>
-      <span>{`Создан ${createdAt}`}</span>
-      <p>{`Последняя операция ${lastOperation} `}<span>{`(${lastOperationDiff} ${currencySymbol})`}</span></p>
+      <strong className={styles.money}>{`${money} ${currencySymbol}`}</strong>
+      <span className={styles.text}>{`${percent}% годовых`}</span>
+      <span className={styles.text}>{`Создан ${createdAt}`}</span>
+      <span className={styles.text}>{`Последняя операция ${lastOperation} `}<span>{`(${lastOperationDiff} ${currencySymbol})`}</span></span>
     </div>
   );
 };
--- src/components/organisms/BankAccount/BankAccount.js
+++ src/components/organisms/BankAccount/BankAccount.js
@@ -6,24 +6,48 @@ import Card from '../../molecules/Card/Card';
 const BankAccount = () => {
   return (
     <div className={styles.wrapper}>
-      <Toggle />
-      <Card
-        accountNumber="57890456"
-        quantity="69 650"
-        percent="8"
-        createdAt="23.01.2017 | 13:55"
-        lastOperation="08.03.2017 | 19:21"
-        lastOperationDiff="+ 3 500"
-      />
-      <Card
-        accountNumber="57890457"
-        quantity="3 500"
-        percent="2"
-        createdAt="23.01.2017 | 13:55"
-        lastOperation="08.03.2017 | 19:21"
-        lastOperationDiff="- 69"
-        currencySymbol="$"
-      />
+      <div className={styles.toggleWrapper}>
+        <Toggle />
+      </div>
+      <div className={styles.cardWrapper}>
+        <Card
+          accountNumber="57890456"
+          money="69 650"
+          percent="8"
+          createdAt="23.01.2017 | 13:55"
+          lastOperation="08.03.2017 | 19:21"
+          lastOperationDiff="+ 3 500"
+        />
+        <Card
+          accountNumber="57890457"
+          money="3 500"
+          percent="2"
+          createdAt="23.01.2017 | 13:55"
+          lastOperation="08.03.2017 | 19:21"
+          lastOperationDiff="- 69"
+          currencySymbol="$"
+          className={styles.greyCard}
+        />
+        <Card
+          accountNumber="57890457"
+          money="3 500"
+          percent="2"
+          createdAt="23.01.2017 | 13:55"
+          lastOperation="08.03.2017 | 19:21"
+          lastOperationDiff="- 69"
+          currencySymbol="$"
+        />
+        <Card
+          accountNumber="57890457"
+          money="3 500"
+          percent="2"
+          createdAt="23.01.2017 | 13:55"
+          lastOperation="08.03.2017 | 19:21"
+          lastOperationDiff="- 69"
+          currencySymbol="$"
+          className={styles.greyCard}
+        />
+      </div>
     </div>
   );
 };

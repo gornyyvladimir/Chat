9b63e9828e5bd0b581eebb88e685d8b7934283cb
Gornyy Vladimir <vladimir.gornyy@flatstack.com>
2018-11-28 17:46:36 +0300

    Add bank account to main page

--- src/components/pages/MainPage/MainPage.js
+++ src/components/pages/MainPage/MainPage.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Chat from '../../organisms/Chat/Chat';
+import BankAccount from '../../organisms/BankAccount/BankAccount';
 import styles from './MainPage.module.css';
 
 const MainPage = props => {
   return (
     <div className={styles.wrapper}>
       <Chat />
+      <BankAccount />
     </div>
   );
 };

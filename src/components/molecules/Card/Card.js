95ce039baae907e76e834d44d9951babbafce2bf
Gornyy Vladimir <vladimir.gornyy@flatstack.com>
2018-11-30 19:56:06 +0300

    Add operations

--- src/components/molecules/Card/Card.js
+++ src/components/molecules/Card/Card.js
@@ -1,58 +1,93 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import classnames from 'classnames';
 import styles from './Card.module.css';
 import ColoredMoney from '../../atoms/ColoredMoney/ColoredMoney';
 import MoneyFormatter from '../../atoms/MoneyFormatter/MoneyFormatter';
 import Operation from '../../molecules/Operation/Operation';
+import {
+  getOperations,
+  resetOperations,
+} from '../../../store/operations/actions';
 
-const Card = ({
-  className,
-  accountNumber,
-  money,
-  percent,
-  createdAt,
-  lastOperation,
-  lastOperationDiff,
-  currencyType = 'RUB',
-}) => {
-  const classes = classnames(styles.wrapper, className);
+class Card extends Component {
+  handleMaximizeClick = () => {
+    this.props.onGetOperations();
+  };
 
-  return (
-    <>
-    <div className={classes}>
-      <div className={styles.headerWrapper}>
-        <h3 className={styles.accountNumber}>{`Счет № ${accountNumber} `}</h3>
-        <button
-          className={styles.maximizeButton}
-          onClick={() => console.log('Clcik')}
-        >
-          Full size
-        </button>
-      </div>
-      <MoneyFormatter money={money} currencyType={currencyType}>
-        {formattedMoney => (
-          <strong className={styles.money}>{formattedMoney}</strong>
-        )}
-      </MoneyFormatter>
-      <span className={styles.text}>{`${percent}% годовых`}</span>
-      <span className={styles.text}>{`Создан ${createdAt}`}</span>
-      <span className={styles.text}>
-        {`Последняя операция ${lastOperation} `}(
-        <MoneyFormatter
-          money={lastOperationDiff}
-          currencyType={currencyType}
-          isDiff
-        >
-          {formattedMoney => <ColoredMoney>{formattedMoney}</ColoredMoney>}
-        </MoneyFormatter>
-        )
-      </span>
-    </div>
-    <Operation date="08.03.2017 | 19:21" operation="Пополнение с карты ****0465" operationDiff={3500} currencyType="RUB" />
-    <Operation date="08.03.2017 | 19:21" operation="Пополнение с карты ****0465" operationDiff={3500} currencyType="RUB" />
-    <Operation date="08.03.2017 | 19:21" operation="Пополнение с карты ****0465" operationDiff={3500} currencyType="RUB" />
-    </>
-  );
+  render() {
+    const {
+      className,
+      accountNumber,
+      money,
+      percent,
+      createdAt,
+      lastOperation,
+      lastOperationDiff,
+      operations,
+      currencyType = 'RUB',
+    } = this.props;
+
+    const classes = classnames(styles.wrapper, className);
+
+    return (
+      <>
+        <div className={classes}>
+          <div className={styles.headerWrapper}>
+            <h3
+              className={styles.accountNumber}
+            >{`Счет № ${accountNumber} `}</h3>
+            <button
+              className={styles.maximizeButton}
+              onClick={this.handleMaximizeClick}
+            >
+              Full size
+            </button>
+          </div>
+          <MoneyFormatter money={money} currencyType={currencyType}>
+            {formattedMoney => (
+              <strong className={styles.money}>{formattedMoney}</strong>
+            )}
+          </MoneyFormatter>
+          <span className={styles.text}>{`${percent}% годовых`}</span>
+          <span className={styles.text}>{`Создан ${createdAt}`}</span>
+          <span className={styles.text}>
+            {`Последняя операция ${lastOperation} `}(
+            <MoneyFormatter
+              money={lastOperationDiff}
+              currencyType={currencyType}
+              isDiff
+            >
+              {formattedMoney => <ColoredMoney>{formattedMoney}</ColoredMoney>}
+            </MoneyFormatter>
+            )
+          </span>
+        </div>
+        {!!operations.length &&
+          operations.map(operation => (
+            <Operation
+              key={operation.id}
+              date={operation.createdAt}
+              operation={operation.operation}
+              operationDiff={operation.operationDiff}
+              currencyType={operation.currencyType}
+            />
+          ))}
+      </>
+    );
+  }
+}
+
+const mapStateToProps = state => ({
+  operations: state.operations.operations,
+});
+
+const mapDispatchToProps = {
+  onGetOperations: bankAccount => getOperations(bankAccount),
+  onResetOperations: () => resetOperations(),
 };
 
-export default Card;
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps,
+)(Card);
